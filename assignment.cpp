4d67f9cf0dfc939452b540d9f99ad3d61bee0458
Nicholas Chan <nicholas.chan002@binus.ac.id>
2025-09-06 15:47:49 +0700

    Added assignment file

---
+++ assignment.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Name : Nicholas Chan" << std::endl;
+    std::cout << "Favourite Programming Language : C++" << std::endl;
+    system("PAUSE");
+    return 0;
+}
+
